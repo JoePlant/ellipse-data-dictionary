659221159e6df4900a6a035d24905d52d959bb69
Joseph Plant <git@JosephPlant.com>
2014-01-21 11:29:49 +1000

    Save Parsed results to file

--- src/Ellipse.Parser.Tests/System/CobolHierarchySystemTests.cs
+++ src/Ellipse.Parser.Tests/System/CobolHierarchySystemTests.cs
@@ -22,121 +22,141 @@ namespace Ellipse.DataDictionary.System
         [Test]
         public void MSF001_RECORD()
         {
-            ParseHierarchicalFile(@".\Resources\Cobol\MSF001-RECORD.rpt");
+            IModel model = ParseHierarchicalFile(@".\Resources\Cobol\MSF001-RECORD.rpt");
+            SaveAsXml(model);
         }
 
         [Test]
         public void MSF002_RECORD()
         {
-            ParseHierarchicalFile(@".\Resources\Cobol\MSF002-RECORD.rpt");
+            IModel model = ParseHierarchicalFile(@".\Resources\Cobol\MSF002-RECORD.rpt");
+            SaveAsXml(model);
         }
 
         [Test]
         public void MSF003_RECORD()
         {
-            ParseHierarchicalFile(@".\Resources\Cobol\MSF003-RECORD.rpt");
+            IModel model = ParseHierarchicalFile(@".\Resources\Cobol\MSF003-RECORD.rpt");
+            SaveAsXml(model);
         }
 
         [Test]
         public void MSF004_RECORD()
         {
-            ParseHierarchicalFile(@".\Resources\Cobol\MSF004-RECORD.rpt");
+            IModel model = ParseHierarchicalFile(@".\Resources\Cobol\MSF004-RECORD.rpt");
+            SaveAsXml(model);
         }
 
         [Test]
         public void MSF005_RECORD()
         {
-            ParseHierarchicalFile(@".\Resources\Cobol\MSF005-RECORD.rpt");
+            IModel model = ParseHierarchicalFile(@".\Resources\Cobol\MSF005-RECORD.rpt");
+            SaveAsXml(model);
         }
 
         [Test]
         public void MSF006_RECORD()
         {
-            ParseHierarchicalFile(@".\Resources\Cobol\MSF006-RECORD.rpt");
+            IModel model = ParseHierarchicalFile(@".\Resources\Cobol\MSF006-RECORD.rpt");
+            SaveAsXml(model);
         }
 
         [Test]
         public void MSF007_RECORD()
         {
-            ParseHierarchicalFile(@".\Resources\Cobol\MSF007-RECORD.rpt");
+            IModel model = ParseHierarchicalFile(@".\Resources\Cobol\MSF007-RECORD.rpt");
+            SaveAsXml(model);
         }
 
         [Test]
         public void MSF008_RECORD()
         {
-            ParseHierarchicalFile(@".\Resources\Cobol\MSF008-RECORD.rpt");
+            IModel model = ParseHierarchicalFile(@".\Resources\Cobol\MSF008-RECORD.rpt");
+            SaveAsXml(model);
         }
 
         [Test]
         public void MSF010_RECORD()
         {
-            ParseHierarchicalFile(@".\Resources\Cobol\MSF010-RECORD.rpt");
+            IModel model = ParseHierarchicalFile(@".\Resources\Cobol\MSF010-RECORD.rpt");
+            SaveAsXml(model);
         }
 
         [Test]
         public void MSF011_RECORD()
         {
-            ParseHierarchicalFile(@".\Resources\Cobol\MSF011-RECORD.rpt");
+            IModel model = ParseHierarchicalFile(@".\Resources\Cobol\MSF011-RECORD.rpt");
+            SaveAsXml(model);
         }
 
         [Test]
         public void MSF012_RECORD()
         {
-            ParseHierarchicalFile(@".\Resources\Cobol\MSF012-RECORD.rpt");
+            IModel model = ParseHierarchicalFile(@".\Resources\Cobol\MSF012-RECORD.rpt");
+            SaveAsXml(model);
         }
 
         [Test]
         public void MSF013_RECORD()
         {
-            ParseHierarchicalFile(@".\Resources\Cobol\MSF013-RECORD.rpt");
+            IModel model = ParseHierarchicalFile(@".\Resources\Cobol\MSF013-RECORD.rpt");
+            SaveAsXml(model);
         }
 
         [Test]
         public void MSF014_RECORD()
         {
-            ParseHierarchicalFile(@".\Resources\Cobol\MSF014-RECORD.rpt");
+            IModel model = ParseHierarchicalFile(@".\Resources\Cobol\MSF014-RECORD.rpt");
+            SaveAsXml(model);
         }
 
         [Test]
         public void MSF015_RECORD()
         {
-            ParseHierarchicalFile(@".\Resources\Cobol\MSF015-RECORD.rpt");
+            IModel model = ParseHierarchicalFile(@".\Resources\Cobol\MSF015-RECORD.rpt");
+            SaveAsXml(model);
         }
 
         [Test]
         public void MSF016_RECORD()
         {
-            ParseHierarchicalFile(@".\Resources\Cobol\MSF016-RECORD.rpt");
+            IModel model = ParseHierarchicalFile(@".\Resources\Cobol\MSF016-RECORD.rpt");
+            SaveAsXml(model);
         }
 
         [Test]
         public void MSF017_RECORD()
         {
-            ParseHierarchicalFile(@".\Resources\Cobol\MSF017-RECORD.rpt");
+            IModel model = ParseHierarchicalFile(@".\Resources\Cobol\MSF017-RECORD.rpt");
+            SaveAsXml(model);
         }
 
         [Test]
         public void MSF018_RECORD()
         {
-            ParseHierarchicalFile(@".\Resources\Cobol\MSF018-RECORD.rpt");
+            IModel model = ParseHierarchicalFile(@".\Resources\Cobol\MSF018-RECORD.rpt");
+            SaveAsXml(model);
         }
 
         [Test]
         public void MSF019_RECORD()
         {
-            ParseHierarchicalFile(@".\Resources\Cobol\MSF019-RECORD.rpt");
+            IModel model = ParseHierarchicalFile(@".\Resources\Cobol\MSF019-RECORD.rpt");
+            SaveAsXml(model);
         }
 
         [Test]
         public void MSF01A_RECORD()
         {
-            ParseHierarchicalFile(@".\Resources\Cobol\MSF01A-RECORD.rpt");
+            IModel model = ParseHierarchicalFile(@".\Resources\Cobol\MSF01A-RECORD.rpt");
+            SaveAsXml(model);
         }
 
         [Test]
         public void MSF01B_RECORD()
         {
-            ParseHierarchicalFile(@".\Resources\Cobol\MSF01B-RECORD.rpt");
+            IModel model = ParseHierarchicalFile(@".\Resources\Cobol\MSF01B-RECORD.rpt");
+            SaveAsXml(model);
         }
 
         /// <summary>
@@ -145,19 +165,22 @@ namespace Ellipse.DataDictionary.System
         [Test]
         public void MSF023_RECORD()
         {
-            ParseHierarchicalFile(@".\Resources\Cobol\MSF023-RECORD.rpt");
+            IModel model = ParseHierarchicalFile(@".\Resources\Cobol\MSF023-RECORD.rpt");
+            SaveAsXml(model);
         }
 
         [Test]
         public void MSF062_RECORD()
         {
-            ParseHierarchicalFile(@".\Resources\Cobol\MSF062-RECORD.rpt");
+            IModel model = ParseHierarchicalFile(@".\Resources\Cobol\MSF062-RECORD.rpt");
+            SaveAsXml(model);
         }
 
         [Test]
         public void MSW000_RECORD()
         {
-            ParseHierarchicalFile(@".\Resources\Cobol\MSW000-RECORD.rpt");
+            IModel model = ParseHierarchicalFile(@".\Resources\Cobol\MSW000-RECORD.rpt");
+            SaveAsXml(model);
         }
 
     }
